f3a74dcd9fb679b1e35beb803f9d01efb537a229
actiontwo <actiontwo@designveloper.com>
2015-01-30 16:45:02 +0700

    first init

---
+++ disable-plugin-updates/Function.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+
+ * @package Disable Plugin Updates
+ * @author Websiteguy
+
+ * @version 0.5
+*/
+
+/*
+
+Plugin Name: Disable Plugin Updates
+
+Plugin URI: http://wordpress.org/plugins/disable-plugin-updates/
+Version: 0.5
+Description: A simple WordPress plugin that disables all the updating of plugins.
+Author: <a href="http://profiles.wordpress.org/kidsguide">Websiteguy</a>
+Author URL: http://profiles.wordpress.org/kidsguide
+Compatible with WordPress 2.3+.
+
+
+*/
+
+
+/*
+
+Copyright 2013 Websiteguy (email : mpsparrow@cogeco.ca)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+
+// Hides all upgrade notices
+add_action('admin_menu','hide_admin_notices');
+function hide_admin_notices() {
+    remove_action( 'admin_notices', 'update_nag', 3 );
+}
+
+// Disable plugin updates
+remove_action( 'load-update-core.php', 'wp_update_plugins' );
+add_filter( 'pre_site_transient_update_plugins', create_function( '$a', "return null;" ) );
+
---
+++ disable-plugin-updates/readme.txt
@@ -0,0 +1,57 @@
+=== Disable Plugin Updates ===
+
+Contributors: kidsguide
+Tags: Plugin Updates, Disable Plugins, Disable Plugin Updates
+Requires at least: 2.4
+Tested up to: 3.8
+Stable tag: trunk
+
+<strong>This plugin is no longer under development</strong> A simple WordPress plugin that disables all the updating of plugins.
+
+
+
+== Description ==
+
+A simple WordPress plugin that disables all the updating of plugins.
+
+This plugin is no longer under development because it has been combined with: <a href="http://wordpress.org/plugins/stops-core-theme-and-plugin-updates">Disable Updates Manager</a>
+
+
+
+= Please note!!! =
+
+
+It is important to keep your plugins up to date. 
+If you use this plugin, make sure your plugins is not harming your website.
+
+== Installation ==
+
+
+1. Go to the Plugins> Add New> Upload in you admin dashboard.
+
+2. Upload the zip file and install.
+3. Go to the Plugins section in your admin dashboard and activate.
+
+== Changelog ==
+
+
+= 0.5 =
+* End of development. This plugin is no longer under development because it has been combined with: <a href="http://wordpress.org/plugins/stops-core-theme-and-plugin-updates">Disable Updates Manager</a>
+
+= 0.4 =
+* Remove Update Menu Code Removed
+* Description Updated
+* Works with WordPress Version 3.8
+
+= 0.3 =
+* Updated Readme.txt  
+
+= 0.2 =
+* Updated Readme.txt  
+
+= Versions Not Available for Downloading = 
+
+= 0.1 =
+ 
+* Published on Wordpress.org
+ (October 10, 2013) 
